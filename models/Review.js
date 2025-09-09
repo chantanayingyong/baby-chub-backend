2a52050a077af1befe2f7f7858223f69548a3864
suchakree <chantanayingyong.s@gmail.com>
2025-09-09 15:23:44 +0700

    feat: review product api routes

--- models/Review.js
+++ models/Review.js
@@ -1,7 +1,36 @@
 import { Schema, model } from "mongoose";
 
-const ReviewSchema = new Schema({
-
-});
+const ReviewSchema = new Schema(
+  {
+    productId: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: false, //ชั่วคราว จนกว่า user routes จะเสร็จ
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      default: 5,
+    },
+    comment: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    isPinned: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const Review = model("Review", ReviewSchema);
