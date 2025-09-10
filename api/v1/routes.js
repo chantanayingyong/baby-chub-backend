671541d2e18614b2769263aa1e9107076ccb9035
Man-metee <metees.aira@gmail.com>
2025-09-10 22:07:14 +0700

    pre merge

--- api/v1/routes.js
+++ api/v1/routes.js
@@ -1,9 +1,14 @@
 import express from "express";
 import productRoutes from "./routes/productRoutes.js"
+import authRoutes from "./routes/authRoutes.js";   // authRoutes ไม่มี prefix
+import userRoutes from "./routes/userRoutes.js";   // มี /users ในไฟล์แล้ว
 
 export default () => {
   const router = express.Router();
 
   router.use("/", productRoutes);
+  router.use("/auth", authRoutes);
+  router.use("/", userRoutes);
+
   return router;
 };
