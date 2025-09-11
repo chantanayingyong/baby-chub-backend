64e07d22941585d852c48329f0050807f37598b3
Anonyhito <108658785+Anonyhito@users.noreply.github.com>
2025-09-11 13:14:29 +0700

    try pull

--- api/v1/routes.js
+++ api/v1/routes.js
@@ -7,11 +7,8 @@ export default () => {
   const router = express.Router();
 
   router.use("/", productRoutes);
-<<<<<<< HEAD
-=======
   router.use("/auth", authRoutes);
   router.use("/", userRoutes);
 
->>>>>>> 86690e93ecf22b266acc361f86b96704eef6073f
   return router;
 };
