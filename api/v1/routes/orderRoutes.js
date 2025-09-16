659a8f2fe51512c20f128771a05b3acc0708514e
supakkit <87434100+supakkit@users.noreply.github.com>
2025-09-16 11:20:37 +0700

    add checking admin role before into some order routes

--- api/v1/routes/orderRoutes.js
+++ api/v1/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createOrder, deleteOrder, getOrderById, getOrders, getOrdersByUserId, patchOrderStatus, updateOrderStatus } from "../controllers/ordersController.js";
-
+import { requireAdmin } from "../../../middleware/auth.js";
 
 const router = express.Router();
 
@@ -8,10 +8,10 @@ router.get("/order", getOrdersByUserId);
 router.post("/order", createOrder);
 router.patch("/order/:orderId", updateOrderStatus);
 
-router.get("/admin/orders", getOrders);
+router.get("/admin/orders", requireAdmin, getOrders);
 router.get("/order/:orderId", getOrderById);
-router.patch("/admin/order/:orderId", patchOrderStatus);
-router.delete("/admin/order/:orderId", deleteOrder);
+router.patch("/admin/order/:orderId", requireAdmin, patchOrderStatus);
+router.delete("/admin/order/:orderId", requireAdmin, deleteOrder);
 
 
 export default router;
